9501ab5cc78c4c35be4c7f0b417d82507e35dac2
David Torrejano <davitorrejano@gmail.com>
2024-11-25 21:58:55 -0500

    Navigable map and its methods

--- src/collections/framework/Maps.java
+++ src/collections/framework/Maps.java
@@ -1,9 +1,6 @@
 package collections.framework;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+import java.util.*;
 
 // Map doesn't extend from collection, although it can be seen as a collection
 public class Maps {
@@ -16,22 +13,37 @@ public class Maps {
         map.put("Rodrigo", 100);
         map.put("Armaní", 1000);
 
-        System.out.println(map.getOrDefault("David", 7));
-        System.out.println(map.getOrDefault("Rodrigo", 5));
+//        System.out.println(map.getOrDefault("David", 7));
+//        System.out.println(map.getOrDefault("Rodrigo", 5));
 
 
         Set<Map.Entry<String, Integer>> entrySet =  map.entrySet();
 
-        for (Map.Entry<String, Integer> entry : entrySet) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+//        for (Map.Entry<String, Integer> entry : entrySet) {
+//            System.out.println(entry.getKey() + " " + entry.getValue());
+//        }
 
         Set <String> allKeys = map.keySet();
-        for (String key : allKeys) {
-            System.out.println("{" + key + ": " + map.get(key) + "}");
-        }
+//        for (String key : allKeys) {
+//            System.out.println("{" + key + ": " + map.get(key) + "}");
+//        }
 
-        Map<String, Integer> treeMap = new TreeMap<>();
+        // The treeMap implements SortedMap and NavigableMap, that is why we can have methods to find values such as:
+        // ceilingEntry(K key) -> this will return the value of the lowest key, higher or equal to the key passed as argument
+        // floorEntry(K key) -> Does about the same as ceiling but returns the highest key lower than the passed
+        // as argument or the one passed as argument
+        // The higherEntry(K key) and lowerEntry(K key) don't include the key passed as argument, therefore, even if the
+        // key exists this method will return the closest higher or lower value or if not present, returns null.
+
+        NavigableMap<String, Integer> treeMap = new TreeMap<>((a, b) -> b.compareToIgnoreCase(a));
+        treeMap.put("David", 1);
+        treeMap.put("Fernando", 10);
+        treeMap.put("Rodrigo", 100);
+        treeMap.put("Armaní", 1000);
+
+        Map.Entry<String, Integer> nm = treeMap.ceilingEntry("J");
+        System.out.println(nm);
+        System.out.println(treeMap.ceilingKey("Z"));
 
 
 
